17ab327b8b370b878b2d31b76921cfe4d803dba5
Ben Thompson <btomo86@gmail.com>
2020-10-07 21:32:22 +0100

    all katas completed

--- src/promise-katas-1.js
+++ src/promise-katas-1.js
@@ -10,33 +10,59 @@ However, you will have to amend the body of the functions themselves in order to
 // Example:
 // returnPromise() resolves
 
-const returnPromise = () => { };
+const returnPromise = () => {
+    const promise = new Promise((resolve,reject) => {
+        resolve();
+      });
+      return promise;
+    };
 
-// 2 Create a function that returns a promise which returns 10 if it resolves succesfully
-// Example:
-// returnTen() resolves to 10
 
-const returnTen = () => { };
 
-// 3 Create a function that returns a promise which returns "An error occurred" if it rejects
-// Example:
-// anError() rejects to "An error occurred"
+const returnTen = () => {
+    const Ten = new Promise((resolve, reject) => {
+      resolve(10);
+    });
+    return Ten;
+  };
 
-const anError = () => { };
 
-// 4 Create a function that returns a promise which resolves to "happy" if the parameter is greater than 
-// or equal to 1 and rejects to "sad" if the value is less than 1
-// Example:
-// happySad(1) resolves to "happy"
-// happySad(0) rejects to "sad"
+const anError = () => { 
+  const promise = new Promise((resolve, reject) => {
+    reject("An error occurred");
+})
+return promise;
+};
 
-const happySad = () => { };
+const happySad = (x) => {
+    return new Promise((resolve, reject) => {
+      if (x >=1) {
+        resolve("happy");
+      } else {
+        reject("sad");
+      }
+    });
+  };
 
 // 5 Create a function that returns a promise which resolves to a function that returns "My name is <input name>" 
 // Example:
 // myNameIs() resolves to a function that takes one argument, name, and returns "My name is name" Eg name => `Me name is ${name}`
 
-const myNameIs = () => { };
+const myNameIs = () => { 
+    const name = new Promise ((resolve, reject) => {
+        resolve((name) => {
+        return `My name is ${name}`;
+          });
+        });
+        return name;
+      };
+
+
+
+
+
+
+
 
 
 module.exports = {
--- src/promise-katas-2.js
+++ src/promise-katas-2.js
@@ -12,17 +12,29 @@ You can look at the fakeApi to see how the data is returned
 // 1 Create a function that uses the getData function to make a request to the "food" URL and returns
 // the data - expected return value "Cheese" of type String
 
-const food = () => {};
+const food = () => {
+    return getData("food").then((res) => res.data);
+        
+};
 
 // 2 Create a function that uses the getData function to make a request to the "cats" URL and returns
 // a list of cats in alphabetical order - expected return value ["Bandit", "Berry", "Puss in boots", "Smokey"] of type Array
 
-const cat = () => {};
+const cat = () => {
+    return getData('cats').then((res) => res.data.cats.sort());
+   
+};
 
 // 3 Create a function that uses the getData function to make a request to the "dogs" URL and returns
 // the naughtiest dog - expected return value {name: "Mutley", naughty: 10} of type Object
 
-const dog = () => {};
+const dog = () => {
+    return getData('dogs').then((res) => {
+        const result = res.data.dogs.filter(n => n.naughty >= 10)[0];
+        return result;
+    })
+
+};
 
 // 4 Create a function that uses the getData function to make requests to the "jokes" URL and returns
 // a joke object with the key of question and answer - expected return { 
@@ -30,7 +42,13 @@ const dog = () => {};
 //     answer: "Because he was out-standing in his field."
 // } of type Object
 
-const joke = () => {};
+const joke = () => {
+    return Promise.all([getData("jokes"), getData("jokes", "question")]).then(
+          ([answerObj, jokesObj]) => {
+            return { ...answerObj, question: jokesObj.joke };
+          }
+        );
+};
 
 module.exports = {
     food,
